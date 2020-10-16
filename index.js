7bc2737dbe3bb1a4d9c3cd0a73f233aaf22ec341
Blair MacIntyre <blair@macmynatt.com>
2020-10-15 20:29:47 -0400

    lets encrypt stuff

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express()
 
 app.use(express.static('public'))
 
-app.get('/.well-known/acme-challenge/uubkcfjSd8v_ATBFo-ADSrbc68LVZYMQ2ZvU-9Asf2c', (req,res) => {
-    res.send("uubkcfjSd8v_ATBFo-ADSrbc68LVZYMQ2ZvU-9Asf2c.VeFbm-Pcx9jG1LNNYKt1-ssk8U1QMse-QJsLzcWPGiI")
+app.get('/.well-known/acme-challenge/sdk7Ne4KyfDw6dLwD39qIqJ8BcFiAWTLYLeZjhE9ylc', (req,res) => {
+    res.send("sdk7Ne4KyfDw6dLwD39qIqJ8BcFiAWTLYLeZjhE9ylc.VeFbm-Pcx9jG1LNNYKt1-ssk8U1QMse-QJsLzcWPGiI")
 })
   
 app.listen(3000, () => console.log('Server running on port 3000'))
